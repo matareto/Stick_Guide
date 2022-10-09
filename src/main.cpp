c0950fe649b44699427413ab545d509e2ff09f54
Rubén Amado <rdar1997@gmail.com>
2022-10-08 21:48:38 -0500

    Ultrasonico Programado

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,33 @@
 
 #define trig 14
 #define echo 27
-#define led 2
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(trig,OUTPUT);
-  pinMode(echo,INPUT);
-  pinMode(led,OUTPUT);
+int distancia = 0;
+
+long distanciaUltrasonico(int gat, int eco){
+  pinMode(gat, OUTPUT);
+  digitalWrite(gat, LOW);
+  delayMicroseconds(2);
+  digitalWrite(gat, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(gat, LOW);
+
+  pinMode(eco, INPUT);
+
+  return pulseIn(eco, HIGH);
+}
+void setup()
+{
+  Serial.begin(9600); 
 }
 
-void loop() {
-  digitalWrite(led,HIGH);
-  delay(1000);
-  digitalWrite(led,LOW);
+void loop()
+{
+
+  distancia = 0.032/2 * distanciaUltrasonico(trig,echo);
+
+  Serial.print("Distancia: ");
+  Serial.print(distancia );
+  Serial.println(" cm");
   delay(1000);
 }
